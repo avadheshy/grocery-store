37fd3cfbb75f13c14c69c4d435003e63743320b9
avadheshy <avadheshy2015@gmail.com>
2022-07-27 17:17:48 +0530

    adding mysql

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = 'mysql://avadhesh:@1997Avdy@localhost/grocery'
-DATABASE_URL = 'sqlite:///mydata.db'
+DATABASE_URL = "mysql+pymysql://database:@1997Avdy@host/grocery"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
