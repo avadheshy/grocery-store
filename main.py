c6179bd4f7d4892774dcd1c7be2101033c5535cb
avadheshy <avadheshy2015@gmail.com>
2022-07-29 11:43:30 +0530

    template added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body, Depends
+from fastapi import FastAPI, Body, Depends,Request
+from fastapi.templating import Jinja2Templates
 import schemas
 import models
 
@@ -6,7 +7,7 @@ from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session 
 
 Base.metadata.create_all(engine)
-
+templates=Jinja2Templates(directory='template')
 def get_session():
     session = SessionLocal()
     try:
@@ -16,30 +17,20 @@ def get_session():
 
 app = FastAPI()
 
-# fakeDatabase = {
-#     1:{'task':'Clean car'},
-#     2:{'task':'Write blog'},
-#     3:{'task':'Start stream'},
-# }
+
 
 @app.get("/")
-def getItems(session: Session = Depends(get_session)):
+def getItems(request:Request,session: Session = Depends(get_session)):
     items = session.query(models.Item).all()
-    return items
+    
+    return templates.TemplateResponse("home.html",{'request':request,'items':items})
 
 @app.get("/{id}")
 def getItem(id:int, session: Session = Depends(get_session)):
     item = session.query(models.Item).get(id)
     return item
 
-#option #1
-# @app.post("/")
-# def addItem(task:str):
-#     newId = len(fakeDatabase.keys()) + 1
-#     fakeDatabase[newId] = {"task":task}
-#     return fakeDatabase
 
-#Option #2
 @app.post("/")
 def addItem(item:schemas.Item, session: Session = Depends(get_session)):
     item = models.Item(task = item.task)
@@ -49,12 +40,6 @@ def addItem(item:schemas.Item, session: Session = Depends(get_session)):
 
     return item
 
-#Option #3
-# @app.post("/")
-# def addItem(body = Body()):
-#     newId = len(fakeDatabase.keys()) + 1
-#     fakeDatabase[newId] = {"task":body['task']}
-#     return fakeDatabase
 
 
 @app.put("/{id}")
