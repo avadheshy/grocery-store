f4534925abf5172c5b46d1ebcc0a20017c2a10a1
avadheshy <avadheshy2015@gmail.com>
2022-07-29 14:05:40 +0530

    base html added

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import Column, Integer, String
 from database import Base 
 
@@ -5,3 +6,38 @@ class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True)
     task = Column(String(256))
+"""
+class User(Base):
+    __tablename__='users'
+    username=Column(String(256),primary_key=True,unique=True)
+    first_name=Column(String(256))
+    last_name=Column(String(256))
+    email = Column(String(50), unique=True)
+    phone = Column(Integer,unique=True)
+    age=Column(Integer,unique=True)
+    address=Column(String(256))
+
+class Product(Base):
+    ____tablename__='products'
+    name=Column(String(256))
+    id=Column(Integer)
+    category=Column(String(256))
+    image=Column(Integer)
+    price=Column(Integer)
+    description=Column(Integer)
+
+class Order(Base):
+    ____tablename__='orders'
+    id=Column(Integer)
+    user=Column(String(256))
+    product_name=Column(String(256))
+
+class Payment(Base):
+    ____tablename__='payments'
+    id=Column(Integer)
+    user_id=Column(Integer)
+    product_id=Column(Integer)
+    product_name=Column(String(256))
+
+
+"""
